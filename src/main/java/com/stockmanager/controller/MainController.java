4960302dd7a4d3ab6302e7f53b14394b8e78825a
Mathys <M.PeypouxDev@gmail.com>
2026-04-08 15:56:02 +0200

    feat: implement dashboard statistics for products, suppliers, movements & alert threshold

--- src/main/java/com/stockmanager/controller/MainController.java
+++ src/main/java/com/stockmanager/controller/MainController.java
@@ -2,11 +2,16 @@ package com.stockmanager.controller;
 
 import com.stockmanager.service.ProductService;
 import com.stockmanager.model.Product;
+import com.stockmanager.model.StockMovement;
+import com.stockmanager.service.StockMovementService;
+import com.stockmanager.service.SupplierService;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
 import javafx.stage.Stage;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.VBox;
 import org.springframework.stereotype.Component;
@@ -23,6 +28,33 @@ public class MainController {
     private ApplicationContext springContext;
 
     @FXML
+    private TableColumn<StockMovement, String> recentProductColumn;
+
+    @FXML
+    private TableColumn<StockMovement, String> recentTypeColumn;
+
+    @FXML
+    private TableColumn<StockMovement, String> recentQuantityColumn;
+
+    @FXML
+    private TableColumn<StockMovement, String> recentDateColumn;
+
+    @FXML
+    private Label productCountLabel;
+
+    @FXML
+    private Label supplierCountLabel;
+
+    @FXML
+    private Label alertCountLabel;
+
+    @FXML
+    private Label movementCountLabel;
+
+    @FXML
+    private TableView<StockMovement> recentMovementsTable;
+
+    @FXML
     private VBox contentArea;
 
     @FXML
@@ -30,6 +62,12 @@ public class MainController {
 
     @Autowired
     private ProductService productService;
+
+    @Autowired
+    private SupplierService supplierService;
+
+    @Autowired
+    private StockMovementService stockMovementService;
 
     @FXML
     private void showProducts() { loadView("/views/ProductView.fxml"); }
@@ -73,5 +111,43 @@ public class MainController {
         } else {
             alertLabel.setText("Attention " + lowStock.size() + " produit(s) en alerte stock !");
         }
+
+        productCountLabel.setText(String.valueOf(productService.findAll().size()));
+        supplierCountLabel.setText(String.valueOf(supplierService.findAll().size()));
+        alertCountLabel.setText(String.valueOf(lowStock.size()));
+        movementCountLabel.setText(String.valueOf(stockMovementService.findAll().size()));
+
+        List<StockMovement> movements = stockMovementService.findAll();
+
+        recentProductColumn.setCellValueFactory(cellData ->
+                new javafx.beans.property.SimpleStringProperty(
+                        cellData.getValue().getProduct() != null ?
+                                cellData.getValue().getProduct().getName() : ""
+                )
+        );
+
+        recentTypeColumn.setCellValueFactory(cellData ->
+                new javafx.beans.property.SimpleStringProperty(
+                        cellData.getValue().getType() != null ?
+                                cellData.getValue().getType().name() : ""
+                )
+        );
+
+        recentQuantityColumn.setCellValueFactory(cellData ->
+                new javafx.beans.property.SimpleStringProperty(
+                        String.valueOf(cellData.getValue().getQuantity())
+                )
+        );
+
+        recentDateColumn.setCellValueFactory(cellData ->
+                new javafx.beans.property.SimpleStringProperty(
+                        cellData.getValue().getDate() != null ?
+                                cellData.getValue().toString() : ""
+                )
+        );
+
+        recentMovementsTable.setItems(
+                javafx.collections.FXCollections.observableArrayList(movements)
+        );
     }
 }
